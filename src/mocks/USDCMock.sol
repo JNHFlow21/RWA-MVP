5eaa40abd2e157762b9bd063882d3d10d92f9d47
JNHFlow21 <JNHFlow21@users.noreply.github.com>
2025-08-01 08:19:46 +0800

    完成USDCMock.

--- src/mocks/USDCMock.sol
+++ src/mocks/USDCMock.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDCMock is ERC20 {
+    constructor(address admin) ERC20("USD Coin", "USDC") {
+        _mint(admin, 1e12);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
